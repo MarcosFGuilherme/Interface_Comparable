9d8be2f26235117d1723680a07eaa898d0c88e42
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-09 09:31:42 -0300

    Interface comparable - Leitura do Arquivo.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,34 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-
+		String path = "C:\\Temp\\In\\in.txt";
+		
+		List<String> employees = new ArrayList<>();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+			
+			while (line != null) {
+				employees.add(line);
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
+		}
+		for (String emp : employees) {
+			System.out.println(emp);
+		}
 	}
 
 }

a60380c0acc842c03bbdd1c65e9a792b31c7ed32
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-09 09:38:16 -0300

    Interface comparable - Ordenacao da Lista.

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -14,21 +15,23 @@ public class Program {
 		
 		String path = "C:\\Temp\\In\\in.txt";
 		
-		List<String> employees = new ArrayList<>();
+		List<String> list = new ArrayList<>();
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
 			
 			while (line != null) {
-				employees.add(line);
+				list.add(line);
 				line = br.readLine();
+			}
+			Collections.sort(list);
+			for (String s : list) {
+				System.out.println(s);
 			}
 		} catch (IOException e) {
 			System.out.println("Error reading file: " + e.getMessage());
 		}
-		for (String emp : employees) {
-			System.out.println(emp);
-		}
+		
 	}
 
 }
